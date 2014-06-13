d472cb2e68d35851dd5abb9feda4a7ebf35cf691
robsonbrown <git@dixonbrown.com>
2014-06-13 23:20:58 +0100

    - DYNAMIC_ARRAY (Vector) typedef

--- core/definitions/parameter_defs.h
+++ core/definitions/parameter_defs.h
@@ -1,21 +1,36 @@
 #include "qstring.h"
+#include <vector>
+
+#ifndef PARAMETER_DEFS_H
+#define PARAMETER_DEFS_H
 
 //Integers
 
-typedef unsigned char U8;
-typedef char S8;
+typedef unsigned char	U8;
+typedef char			S8;
 
-typedef unsigned short U16;
-typedef short		   S16;
+typedef unsigned short	U16;
+typedef short			S16;
 
-typedef unsigned int U32;
-typedef int			 S32;
+typedef unsigned int	U32;
+typedef int				S32;
 
-typedef unsigned long U64;
-typedef long		  S64;
+typedef unsigned long	U64;
+typedef long			S64;
 
 //Strings
-typedef QString STRING;
+typedef QString			STRING;
+
+//Arrays
+template<typename T>
+using DYNAMIC_ARRAY = std::vector < T >;
+
+
+//template<typename T>
+//class DYNAMIC_ARRAY{
+//	std::vector<T> storageType; 
+//	//typedef typename storageType::iterator iterator;
+//};
 
 
 //Trace
@@ -24,3 +39,6 @@ typedef QString STRING;
 #else
 #define TRACE( format, ... )   printf( "%s::%s(%d)" format, __FILE__, __FUNCTION__,  __LINE__, __VA_ARGS__ )
 #endif
+
+
+#endif //PARAMETER_DEFS_H
--- pirateApp.cpp
+++ pirateApp.cpp
@@ -1,9 +1,9 @@
 #include "pirateApp.h"
 
 #include "classes/people/pirate.h"
+#include "core/definitions/parameter_defs.h"
 #include "core/xml/xml_manager.h"
 #include "singletons/game_data.h"
-#include <iostream>
 
 using namespace std;
 
@@ -41,7 +41,12 @@ namespace PIRATE_APP
 
 		XML_MANAGER::read_xml(tempString);
 
-		ui.lblPirate->setText(tempString);
+		DYNAMIC_ARRAY<STRING> temp_string_array;
+
+		temp_string_array.push_back("TEST");
+		temp_string_array.push_back("HELLO");
+
+		ui.lblPirate->setText(temp_string_array.at(1)  );
 	}
 
 }
